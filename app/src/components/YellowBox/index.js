2a865d235932ff3036aa677448770dbc6f6ea09c
crismantovani <crismantovani87@gmail.com>
2021-02-18 01:19:47 -0300

    Cria base do 'grid' de pedidos

--- app/src/components/YellowBox/index.js
+++ app/src/components/YellowBox/index.js
@@ -1,10 +1,22 @@
 /* eslint-disable react/prop-types */
 import React from 'react';
 
-export default function YellowBox({ divYellowBox }) {
+export default function YellowBox({
+  divYellowBox,
+  divYellowBoxMainMenu,
+  divYellowBoxItens,
+  divYellowBoxSides,
+  divYellowBoxDrinks,
+  divYellowBoxOrder,
+}) {
   return (
-    <div className={divYellowBox}>
-      <h1>YellowBox</h1>
+    // <div className={divYellowBox}>
+    <div className={divYellowBoxMainMenu}><h1>GRID WRAP</h1>
+      {/* <h1>YellowBox</h1> */}
+      <div className={divYellowBoxItens}><h1>ITENS</h1></div>
+      <div className={divYellowBoxSides}><h1>ACOMPANHAMENTOS</h1></div>
+      <div className={divYellowBoxDrinks}><h1>DRINKS</h1></div>
+      <div className={divYellowBoxOrder}><h1>TOTAL</h1></div>
     </div>
   );
 }
--- app/src/pages/MainMenu/index.js
+++ app/src/pages/MainMenu/index.js
@@ -1,9 +1,33 @@
 import { React, Fragment } from 'react';
+import Header from '../../components/Header';
+import logo from '../../images/logo-horizontal-brown.png';
+import YellowBox from '../../components/YellowBox';
 
 export default function MainMenu() {
   return (
     <Fragment>
-      <p>MENU PRINCIPAL</p>
+      <Header
+        headerClass='header-base bg-color-yellow'
+        headerLogoLink='/salao'
+        logoSrc={logo}
+        workAreaClass='header-title-base color-brown'
+        workAreaText='Menu Principal'
+        divLogoutClass='align-right weight-500'
+        workerRoleClass='header-role-base color-brown'
+        workerRoleText={'Atendente'}
+        workerNameClass='header-name-base color-brown'
+        workerNameText={'Jussara Silva'}
+        buttonLogoutClass='button-logout-base bg-color-light color-brown'
+        buttonLogoutOnClick={''}
+      />
+      <YellowBox
+
+        divYellowBoxMainMenu='yellow-box-main-menu'
+        divYellowBoxItens='yellow-box-itens'
+        divYellowBoxSides='yellow-box-sides'
+        divYellowBoxDrinks='yellow-box-drinks'
+        divYellowBoxOrder='yellow-box-order'
+      />
     </Fragment>
   );
 }
