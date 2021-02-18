31b2d2f2288e3454f88ef0d500224e6ad1b1eef3
akemimeka <akemimeka@gmail.com>
2021-02-18 18:28:30 -0300

    Cria estrutura base da página do Salão

--- app/src/pages/Hall/index.js
+++ app/src/pages/Hall/index.js
@@ -2,6 +2,8 @@
 import React, { Fragment } from 'react';
 import Header from '../../components/Header';
 import logo from '../../images/logo-horizontal-brown.png';
+import InputText from '../../components/InputText';
+import Button from '../../components/Button';
 
 export default function Hall() {
   // Function to get and display the username after he's logged in
@@ -47,8 +49,45 @@ export default function Hall() {
         workerNameClass='header-name-base color-brown'
         workerNameText={'Jussara Silva'}
         buttonLogoutClass='button-logout-base bg-color-light color-brown'
-        buttonLogoutOnClick={''}
+      // buttonLogoutOnClick={''}
       />
+
+      <main className='main-container-base main-container-hall'>
+        <div className='info-message color-dark-green'>
+          Selecione uma mesa e digite o nome do cliente
+        </div>
+
+        <div className='tables-grid-container'>
+          <div className='table-size-4'>
+            <p className='table-number'>01</p>
+          </div>
+          <div className='table-size-4'>
+            <p className='table-number'>02</p>
+          </div>
+          <div className='table-size-4'>
+            <p className='table-number'>03</p>
+          </div>
+          <div className='table-size-4'>
+            <p className='table-number'>04</p>
+          </div>
+          <div className='table-size-4'>
+            <p className='table-number'>05</p>
+          </div>
+          <div className='table-size-2'>
+            <p className='table-number'>06</p>
+          </div>
+          <div className='table-size-2'>
+            <p className='table-number'>07</p>
+          </div>
+        </div>
+
+        <section className='client-section'>
+          <InputText
+          />
+          <Button
+          />
+        </section>
+      </main>
     </Fragment>
   );
 }
