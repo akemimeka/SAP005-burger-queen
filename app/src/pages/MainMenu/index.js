8d08ac2f933f89408a4200c1d9eae688a24711b8
akemimeka <akemimeka@gmail.com>
2021-02-18 16:01:13 -0300

    Refatora grid para os menus

--- app/src/pages/MainMenu/index.js
+++ app/src/pages/MainMenu/index.js
@@ -5,25 +5,29 @@ import logo from '../../images/logo-horizontal-brown.png';
 export default function MainMenu() {
   return (
     <Fragment>
-      <Header
-        headerClass='header-base bg-color-yellow'
-        headerLogoLink='/salao'
-        logoSrc={logo}
-        workAreaClass='header-title-base color-brown'
-        workAreaText='Menu Principal'
-        divLogoutClass='align-right weight-500'
-        workerRoleClass='header-role-base color-brown'
-        workerRoleText={'Atendente'}
-        workerNameClass='header-name-base color-brown'
-        workerNameText={'Jussara Silva'}
-        buttonLogoutClass='button-logout-base bg-color-light color-brown'
-        buttonLogoutOnClick={''}
-      />
-      <div className='main-container-base yellow-box-main-menu'>
-        <div className='yellow-box-itens'><h1>ITENS</h1></div>
-        <div className='yellow-box-sides'><h1>ACOMPANHAMENTOS</h1></div>
-        <div className='yellow-box-drinks'><h1>DRINKS</h1></div>
-        <div className='yellow-box-order'><h1>TOTAL</h1></div>
+      <div className='menu-grid-container'>
+        <Header
+          headerClass='header-base header-main-menu bg-color-yellow'
+          headerLogoLink='/salao'
+          logoSrc={logo}
+          workAreaClass='header-title-base color-brown'
+          workAreaText='Menu Principal'
+          divLogoutClass='align-right weight-500'
+          workerRoleClass='header-role-base color-brown'
+          workerRoleText={'Atendente'}
+          workerNameClass='header-name-base color-brown'
+          workerNameText={'Jussara Silva'}
+          buttonLogoutClass='button-logout-base bg-color-light color-brown'
+          buttonLogoutOnClick={''}
+        />
+
+        <div className='menu-grid-child main-menu-burgers bg-color-yellow-20'>BURGS</div>
+
+        <div className='menu-grid-child main-menu-sides bg-color-yellow-20'>ACOMPANHAMENTOS</div>
+
+        <div className='menu-grid-child main-menu-drinks bg-color-yellow-20'>DRINKS</div>
+
+        <div className='menu-grid-child main-menu-orders bg-color-yellow-20'>TOTAL</div>
       </div>
     </Fragment>
   );
