773a1b434070747ce55a0594aea2fe591ac350a2
crismantovani <crismantovani87@gmail.com>
2021-02-18 22:30:02 -0300

    Cria componente MenuItem e coloca as fotos dos itens no menu principal

--- app/src/pages/MainMenu/index.js
+++ app/src/pages/MainMenu/index.js
@@ -1,6 +1,14 @@
 import { React, Fragment } from 'react';
 import Header from '../../components/Header';
+import MenuItem from '../../components/MenuItem';
 import logo from '../../images/logo-horizontal-brown.png';
+import meatBurger from '../../images/menu-photos/burger-meat.png';
+import chickenBurger from '../../images/menu-photos/burger-chicken.png';
+import veggieBurger from '../../images/menu-photos/burger-veggie.png';
+import frenchFries from '../../images/menu-photos/fries.png';
+import onionRings from '../../images/menu-photos/onion-rings.png';
+import water from '../../images/menu-photos/water.png';
+import soda from '../../images/menu-photos/soda.png';
 
 export default function MainMenu() {
   return (
@@ -21,11 +29,49 @@ export default function MainMenu() {
           buttonLogoutOnClick={''}
         />
 
-        <div className='menu-grid-child main-menu-burgers bg-color-yellow-20'>BURGS</div>
+        <div className='menu-grid-child main-menu-burgers bg-color-yellow-20'>
+          <MenuItem
+            menuItemSrc={meatBurger}
+            menuItemDescription='Hambúrguer de Carne'
+            menuItemText='Carne'
+          />
+          <MenuItem
+            menuItemSrc={chickenBurger}
+            menuItemDescription='Hambúrguer de Frango'
+            menuItemText='Frango'
+          />
+          <MenuItem
+            menuItemSrc={veggieBurger}
+            menuItemDescription='Hambúrguer Vegetariano'
+            menuItemText='Vegetariano'
+          />
+        </div>
 
-        <div className='menu-grid-child main-menu-sides bg-color-yellow-20'>ACOMPANHAMENTOS</div>
+        <div className='menu-grid-child main-menu-sides bg-color-yellow-20'>
+          <MenuItem
+            menuItemSrc={frenchFries}
+            menuItemDescription='Fritas'
+            menuItemText='Fritas'
+          />
+          <MenuItem
+            menuItemSrc={onionRings}
+            menuItemDescription='Anéis de cebola'
+            menuItemText='Anéis de cebola'
+          />
+        </div>
 
-        <div className='menu-grid-child main-menu-drinks bg-color-yellow-20'>DRINKS</div>
+        <div className='menu-grid-child main-menu-drinks bg-color-yellow-20'>
+          <MenuItem
+            menuItemSrc={water}
+            menuItemDescription='Água'
+            menuItemText='Água'
+          />
+          <MenuItem
+            menuItemSrc={soda}
+            menuItemDescription='Refrigerante'
+            menuItemText='Refrigerante'
+          />
+        </div>
 
         <div className='menu-grid-child main-menu-orders bg-color-yellow-20'>TOTAL</div>
       </div>
