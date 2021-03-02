8942e1a02493849645881099d603b369fa54e6ff
crismantovani <crismantovani87@gmail.com>
2021-03-01 21:06:43 -0300

    Remove states não usados

--- app/src/pages/Kitchen/index.js
+++ app/src/pages/Kitchen/index.js
@@ -7,35 +7,26 @@ import OrdersCards from '../../components/OrdersCards';
 
 export default function Kitchen() {
   const apiURL = 'https://lab-api-bq.herokuapp.com';
-  const apiProducts = `${apiURL}/order`;
+  const apiOrders = `${apiURL}/orders`;
   const currentUserToken = localStorage.getItem('currentUserToken');
   const currentOrderId = localStorage.getItem('currentOrderId');
   const getTableNumber = localStorage.getItem('currentTable');
   const getClientName = localStorage.getItem('currentClient');
-  const menuHeaderSubtitle = `Mesa ${getTableNumber} · ${getClientName}`;
-  const [products, setProducts] = useState([]);
+  // const menuHeaderSubtitle = `Mesa ${getTableNumber} · ${getClientName}`;
+  const [getAllOrders, setAllOrders] = useState([]);
   const [selectedItem, setSelectedItem] = useState([]);
 
-  const handleAllOrders = {
+  const handleKitchenOrders = {
     method: 'GET',
     headers: { Authorization: currentUserToken },
     path: { orderID: currentOrderId },
   };
 
-  fetch(apiProducts, handleAllOrders)
+  fetch(apiOrders, handleKitchenOrders)
     .then((response) => response.json())
-    .then((data) => setProducts(data))
+    .then((data) => setAllOrders(data))
     .catch((error) => console.log(error));
 
-  const filterByName = (list, name) => {
-    const filteredItem = list.filter((item) => item.name === name || item.name.includes(name));
-    console.log('selected item', filteredItem[0]);
-    setSelectedItem(filteredItem[0]);
-  };
-
-  const onClickItem = (id) => {
-    filterByName(products, id);
-  };
   return (
     <Fragment>
       <div className='kitchen-grid-container'>
@@ -53,7 +44,6 @@ export default function Kitchen() {
         />
         <section className='menu-grid-child todo-orders bg-color-yellow-20'>
           <h3 className='menu-section-title'>Pedidos Pendentes</h3>
-
           <OrdersCards
           />
 
